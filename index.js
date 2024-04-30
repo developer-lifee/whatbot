95e4f5d629e9a0ce30abc4c49f6dc108e2fc46aa
Esteban Avila <eavila@uniempresarial.edu.co>
2024-04-30 17:23:18 -0500

    mapeo de estados de mensajes y dependencias nuevas

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const schedule = require('node-schedule');
 
 async function connectToDatabase() {
   const connection = await mysql.createConnection({
-    host: '200.118.60.37',
+    host: 'mysql.freehostia.com',
     user: 'estavi0_sheerit',
     password: '26o6ssCOA^',
     database: 'estavi0_sheerit'
@@ -67,110 +67,136 @@ const job = schedule.scheduleJob('0 0 * * *', function() { // Se ejecuta todos l
         });
     });
 });
-
 const userStates = new Map();
-
-// uso de la libreria "whatsapp-web.js 1.23.0", cualquier duda o cambio, REVISAR LA DOCUMENTACION <3
-// https://docs.wwebjs.dev/index.html
 
 client.on('message', async (message) => {
     const userId = message.from;
-    
-await message.reply(
-    "Aquí tienes las opciones disponibles:\n" +
-    "1 - Comprar cuenta\n" +
-    "2 - Revisar credenciales\n" +
-    "3 - Precio de mis cuentas\n" +
-    "4 - No puedo acceder a mi cuenta\n" +
-    "5 - Otro\n" +
-    "Por favor, responde *SOLO* con el número de la opción que deseas."
-);
+    const currentState = userStates.get(userId);
 
-   if (message.body.trim() === '1') {
-        // Caso 1: Comprar cuenta
-        let connection;
-        try {
-            connection = await connectToDatabase();
-            const [rows] = await connection.query('SELECT nombre_cuenta, precio FROM lista_maestra ORDER BY id_streaming');
-            if (rows.length > 0) {
-                let replyMessage = "A continuación, te proporciono la información de nuestras ventas de cuentas streaming:\n";
-                rows.forEach(account => {
-                    replyMessage += `- ${account.nombre_cuenta}: $${account.precio}\n`;
-                });
-                await message.reply(replyMessage);
-            } else {
-                await message.reply("Actualmente no tenemos información sobre cuentas disponibles.");
-            }
-        } catch (error) {
-            console.error('Error al buscar en la base de datos:', error);
-            await message.reply("Hubo un error al procesar tu solicitud. Por favor, inténtalo de nuevo más tarde.");
-        } finally {
-            if (connection) {
-                await connection.end();
-            }
-        }
-    } else if (message.body.trim() === '2') {
-        // Caso 2: Revision de credenciales
-        let connection;
-        try {
-            connection = await connectToDatabase();
-            const phoneNumber = userId.replace('@c.us', '');
-            // Buscar el clienteID usando el número de teléfono
-            const [clients] = await connection.query('SELECT clienteID FROM datos_de_cliente WHERE numero = ?', [phoneNumber]);
-            
-            if (clients.length > 0) {
-                let replyMessage = "Estas son tus cuentas actuales:\n";
-                for (const client of clients) {
-                    // Para cada clienteID, buscar en perfil para obtener los idCuenta
-                    const [profiles] = await connection.query('SELECT idCuenta FROM perfil WHERE clienteID = ?', [client.clienteID]);
-                    for (const profile of profiles) {
-                        // Para cada idCuenta, buscar en datoscuenta para obtener los detalles de la cuenta
-                        const [accounts] = await connection.query('SELECT correo, clave FROM datoscuenta WHERE idCuenta = ?', [profile.idCuenta]);
-                        accounts.forEach((account, index) => {
-                            replyMessage += `${index + 1}- ${account.correo} - ${account.clave}\n`;
-                        });
-                    }
-                }
-                await message.reply(replyMessage);
-            } else {
-                await message.reply("No se encontraron cuentas asociadas a tu número.");
-            }
-        } catch (error) {
-            console.error('Error al buscar en la base de datos:', error);
-            await message.reply("Hubo un error al procesar tu solicitud. Por favor, inténtalo de nuevo más tarde.");
-        } finally {
-            if (connection) {
-                await connection.end();
-            }
-        }
-    } else if (message.body.trim() === '4') {
-        // Caso 4: Problemas al ingresar, retorno de imagenes
-        userStates.set(userId, 'seleccionar_servicio');
-        await message.reply(
-            "Entiendo que tienes problemas para acceder a tu cuenta. Por favor, selecciona el número del servicio al que no puedes acceder:\n" +
-            "1 - Netflix\n" +
-            "2 - Amazon\n" +
-            "3 - Disney\n" +
-            "4 - HBO\n" +
-            "5 - Paramount\n" +
-            "6 - Star+\n" +
-            "7 - Spotify\n" +
-            "8 - YouTube\n" +
-            "9 - Crunchyroll\n" +
-            "10 - IPTV\n" +
-            "11 - Xbox Game Pass\n" +
-            "12 - Chat GPT\n" +
-            "Por favor, responde *SOLO* con el número de la opción que deseas."
-        );
-    } else if (userStates.get(userId) === 'seleccionar_servicio') {
-        userStates.delete(userId);
-        await message.reply("ERROR");
-    }
-     else if (message.body.trim() === '5') {
-        // Caso 5: Revision de credenciales
-        await message.reply("Entiendo que tienes una consulta diferente a las opciones brindadas así que un asesor te atenderá lo mas pronto posible, muchas gracias.");
+    // Procesar respuesta basada en el estado actual
+    switch (currentState) {
+        case undefined:
+            // No hay estado registrado, enviar menú inicial
+            userStates.set(userId, 'main_menu'); // Establecer el estado inicial
+            await message.reply(
+                "Aquí tienes las opciones disponibles:\n" +
+                "1 - Comprar cuenta\n" +
+                "2 - Revisar credenciales\n" +
+                "3 - Precio de mis cuentas\n" +
+                "4 - No puedo acceder a mi cuenta\n" +
+                "5 - Otro\n" +
+                "Por favor, responde *SOLO* con el número de la opción que deseas."
+            );
+            break;
+        case 'main_menu':
+            // Procesar la selección del menú principal
+            await handleMainMenuSelection(message, userId);
+            break;
+        case 'seleccionar_servicio':
+            // Manejar la selección del servicio si el estado es 'seleccionar_servicio'
+            userStates.delete(userId); // Limpiar el estado después de manejar
+            await message.reply("ERROR");
+            break;
+        default:
+            // Estado desconocido o no manejado, limpiar estado y reiniciar
+            userStates.delete(userId);
+            await message.reply("No comprendo tu selección. Vamos a empezar de nuevo.");
+            break;
     }
 });
 
-client.initialize();
+async function handleMainMenuSelection(message, userId) {
+    const userSelection = message.body.trim();
+    switch (userSelection) {
+        case '1':
+            // Procesar la compra de cuenta
+            await processBuyAccount(message, userId);
+            break;
+        case '2':
+            // Revisar credenciales
+            await processCheckCredentials(message, userId);
+            break;
+        case '3':
+            // Informar sobre precios
+            await message.reply("Los precios de nuestras cuentas son...");
+            userStates.delete(userId); // Limpiar el estado después de manejar
+            break;
+        case '4':
+            // Problemas para acceder, mostrar opciones de servicio
+            userStates.set(userId, 'seleccionar_servicio');
+            await message.reply("Selecciona el servicio al que no puedes acceder...");
+            break;
+        case '5':
+            // Consulta diferente
+            await message.reply("Un asesor te atenderá lo más pronto posible.");
+            userStates.delete(userId); // Limpiar el estado después de manejar
+            break;
+        default:
+            // Selección no válida
+            await message.reply("Por favor, selecciona una opción válida del menú.");
+            break;
+    }
+}
 
+async function processBuyAccount(message, userId) {
+    let connection;
+    try {
+        connection = await connectToDatabase();
+        const [rows] = await connection.query('SELECT nombre_cuenta, precio FROM lista_maestra ORDER BY id_streaming');
+        if (rows.length > 0) {
+            let replyMessage = "A continuación, te proporciono la información de nuestras ventas de cuentas streaming:\n";
+            rows.forEach(account => {
+                replyMessage += `- ${account.nombre_cuenta}: $${account.precio}\n`;
+            });
+            await message.reply(replyMessage);
+        } else {
+            await message.reply("Actualmente no tenemos información sobre cuentas disponibles.");
+        }
+    } catch (error) {
+        console.error('Error al buscar en la base de datos:', error);
+        await message.reply("Hubo un error al procesar tu solicitud. Por favor, inténtalo de nuevo más tarde.");
+    } finally {
+        if (connection) {
+            await connection.end();
+        }
+    }
+    userStates.delete(userId); // Limpiar el estado después de manejar
+}
+
+async function processCheckCredentials(message, userId) {
+    let connection;
+    try {
+        connection = await connectToDatabase();
+        const phoneNumber = userId.replace('@c.us', '');
+        // Buscar el clienteID usando el número de teléfono
+        const [clients] = await connection.query('SELECT clienteID FROM datos_de_cliente WHERE numero = ?', [phoneNumber]);
+
+        if (clients.length > 0) {
+            let replyMessage = "Estas son tus cuentas actuales:\n";
+            for (const client of clients) {
+                // Para cada clienteID, buscar en perfil para obtener los idCuenta
+                const [profiles] = await connection.query('SELECT idCuenta FROM perfil WHERE clienteID = ?', [client.clienteID]);
+                for (const profile of profiles) {
+                    // Para cada idCuenta, buscar en datoscuenta para obtener los detalles de la cuenta
+                    const [accounts] = await connection.query('SELECT correo, clave FROM datoscuenta WHERE idCuenta = ?', [profile.idCuenta]);
+                    accounts.forEach((account, index) => {
+                        replyMessage += `${index + 1}- ${account.correo} - ${account.clave}\n`;
+                    });
+                }
+            }
+            await message.reply(replyMessage);
+        } else {
+            await message.reply("No se encontraron cuentas asociadas a tu número.");
+        }
+    } catch (error) {
+        console.error('Error al buscar en la base de datos:', error);
+        await message.reply("Hubo un error al procesar tu solicitud. Por favor, inténtalo de nuevo más tarde.");
+    } finally {
+        if (connection) {
+            await connection.end();
+        }
+    }
+    userStates.delete(userId); // Limpiar el estado después de manejar
+}
+
+client.initialize();
